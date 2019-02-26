ec8254f904d7c6abee6a043de455cfc48ae59567
stritti <stephan@st-strittmatter.name>
2019-02-26 22:23:10 +0100

    remove unused imports

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,6 @@
 
 #include <Homie.h>
 
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <RCSwitch.h>
-
 #include "ConstantValues.hpp"
 #include "CurrentValue.hpp"
 
@@ -26,8 +22,8 @@ const int PIN_DS_SOLAR = 16;  // Pin of Temp-Sensor Solar
 const int PIN_DS_POOL  = 17;  // Pin of Temp-Sensor Pool
 const int PIN_RSSWITCH = 18;  // Data-Pin of 433MHz Sender
 
-const int PIN_RELAY_POOL  = 19;
-const int PIN_RELAY_SOLAR = 20;
+const int PIN_RELAY_POOL  = 18;
+const int PIN_RELAY_SOLAR = 19;
 
 
 const int TEMP_READ_INTERVALL = 60;  //Sekunden zwischen Updates der Temperaturen.
@@ -47,92 +43,17 @@ ESP32TemperatureNode  ctrlTemperatureNode("controllerTemp", "Controller Temperat
 RelayModuleNode poolPumpNode("poolPump", "Pool Pump", PIN_RELAY_POOL);
 RelayModuleNode solarPumpNode("solarPump", "Solar Pump", PIN_RELAY_SOLAR);
 
-RCSwitchNode poolPumpeRCNode("poolPumpRC", "Pool Pump RC", PIN_RSSWITCH, "11111", "10000");
-RCSwitchNode solarPumpeRCNode("solarPumpRC", "Solar Pump RC", PIN_RSSWITCH, "11111", "01000");
+//RCSwitchNode poolPumpeRCNode("poolPumpRC", "Pool Pump RC", PIN_RSSWITCH, "11111", "10000");
+//RCSwitchNode solarPumpeRCNode("solarPumpRC", "Solar Pump RC", PIN_RSSWITCH, "11111", "01000");
 
-//RS Switches via 433MHz
-RCSwitch mySwitch = RCSwitch();
 
 CurrentValues currentValues = CurrentValues();
-
-/**
- * Handler for switching pool Pump on/off.
- */
-bool onPoolPumpSwitchHandler(const HomieRange& range, const String& value) {
-  Homie.getLogger() << "〽 poolPumpSwitchOnHandler -> range " << range.index << " value=" << value << endl;
-  bool retval;
-
-  if (value != "true" && value != "false") {
-
-    retval = false;
-  } else {
-
-    if (value == "true") {
-      // Switch on
-      mySwitch.switchOn("11111", "10000");  //todo: make configurable
-    } else {
-      mySwitch.switchOff("11111", "10000");  //todo: make configurable
-    }
-
-    poolPumpNode.setProperty(cSwitch).send(value);
-    bool on = (value == "true");
-    Homie.getLogger() << "Switch is " << (on ? "on" : "off") << endl;
-
-    retval = true;
-  }
-
-  Homie.getLogger() << "〽 poolPumpSwitchOnHandler <-" << retval << endl;
-  return retval;
-}
-
-/**
- * Handler for switching solar Pump on/off.
- */
-bool onSolarPumpSwitchHandler(const HomieRange& range, const String& value) {
-  Homie.getLogger() << "〽 solarPumpSwitchOnHandler -> range " << range.index << " value=" << value << endl;
-
-  bool retval;
-
-  if (value != "true" && value != "false") {
-
-    Homie.getLogger() << "reveived invalid value: " << value << endl;
-    solarPumpNode.setProperty("status").send("reveived invalid value: " + value);
-
-    retval = false;
-
-  } else {
-
-    if (value == "true") {
-      // Switch on
-      mySwitch.switchOn("11111", "01000");  //todo: make configurable
-    } else {
-      mySwitch.switchOff("11111", "01000");  //todo: make configurable
-    }
-
-    const bool on = (value == "true");
-
-    Homie.getLogger() << "Switch is " << (on ? "on" : "off") << endl;
-    solarPumpNode.setProperty(cSwitch).send(value);
-    solarPumpNode.setProperty("status").send("ok");
-
-    retval = true;
-  }
-
-  Homie.getLogger() << "〽 solarPumpSwitchOnHandler <-" << retval << endl;
-  return retval;
-}
 
 /**
  * Homie Setup handler.
  * Only called when wifi and mqtt are connected.
  */
 void setupHandler() {
-
-  poolPumpNode.advertise(cSwitch).setName("Switch").setDatatype(cDataTypBoolean).settable(onPoolPumpSwitchHandler);
-  poolPumpNode.advertise("status").setName("Status");
-
-  solarPumpNode.advertise(cSwitch).setName("Switch").setDatatype(cDataTypBoolean).settable(onSolarPumpSwitchHandler);
-  solarPumpNode.advertise("status").setName("Status");
 
   //default intervall of sending Temperature values
   temperaturePublishIntervalSetting.setDefaultValue(TEMP_READ_INTERVALL).setValidator([](long candidate) {
