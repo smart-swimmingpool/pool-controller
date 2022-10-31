5b753f24335af3dbc9f52093504ff369678bc328
pinsrq <mario.sieg.64@gmail.com>
2022-10-31 15:45:58 +0100

    Update Config.hpp

--- src/Config.hpp
+++ src/Config.hpp
@@ -22,17 +22,19 @@ namespace PoolController
         #endif
     };
 
-    constexpr std::uint8_t D0  { 16 };
-    constexpr std::uint8_t D1  { 5 };
-    constexpr std::uint8_t D2  { 4 };
-    constexpr std::uint8_t D3  { 0 };
-    constexpr std::uint8_t D4  { 2 };
-    constexpr std::uint8_t D5  { 14 };
-    constexpr std::uint8_t D6  { 12 };
-    constexpr std::uint8_t D7  { 13 };
-    constexpr std::uint8_t D8  { 15 };
-    constexpr std::uint8_t D9  { 3 };
-    constexpr std::uint8_t D10 { 1 };
+    #ifdef ESP32
+        constexpr std::uint8_t D0  { 16 };
+        constexpr std::uint8_t D1  { 5 };
+        constexpr std::uint8_t D2  { 4 };
+        constexpr std::uint8_t D3  { 0 };
+        constexpr std::uint8_t D4  { 2 };
+        constexpr std::uint8_t D5  { 14 };
+        constexpr std::uint8_t D6  { 12 };
+        constexpr std::uint8_t D7  { 13 };
+        constexpr std::uint8_t D8  { 15 };
+        constexpr std::uint8_t D9  { 3 };
+        constexpr std::uint8_t D10 { 1 };
+    #endif
 
     /**
      * Interval to temp updates.
